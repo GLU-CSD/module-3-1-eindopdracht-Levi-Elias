2b45e2e5d713e74b99aff35de49e80772c7a622e
Levi-Elias <le.sastro@outlook.com>
2025-02-14 08:45:59 +0100

    copyright

--- assets/js/filter.js
+++ assets/js/filter.js
@@ -3,12 +3,14 @@ document.getElementById('filter-form').addEventListener('submit', function (e) {
 
   const priceRange = document.getElementById('price-range').value;
   const category = document.getElementById('category').value;
+  const shoeType = document.getElementById('shoe-type').value;
 
   const products = document.querySelectorAll('.product');
 
   products.forEach(product => {
     const productPrice = parseInt(product.getAttribute('data-price'));
     const productCategory = product.getAttribute('data-category');
+    const productShoeType = product.getAttribute('data-shoe-type');
 
     let priceMatch = false;
     if (priceRange === 'all') {
@@ -28,8 +30,9 @@ document.getElementById('filter-form').addEventListener('submit', function (e) {
     }
 
     const categoryMatch = category === 'all' || category === productCategory;
+    const shoeTypeMatch = shoeType === 'all' || shoeType === productShoeType;
 
-    if (priceMatch && categoryMatch) {
+    if (priceMatch && categoryMatch && shoeTypeMatch) {
       product.style.display = 'block';
     } else {
       product.style.display = 'none';
